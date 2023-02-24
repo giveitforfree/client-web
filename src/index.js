0e273efc4b368da9497e8bb4b6fc758a01716c60
IKEN Mouad <49724991+IKENMouad@users.noreply.github.com>
2023-02-24 17:02:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "remixicon/fonts/remixicon.css";
